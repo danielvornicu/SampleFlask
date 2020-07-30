6f61d90f7cfd421d78682fd4b045d212c0b2cafe
danielvornicu <vdany2003@yahoo.com>
2020-07-30 18:06:34 +0200

    update

--- app/config.py
+++ app/config.py
@@ -2,6 +2,8 @@ import os
 
 class Config(object):
     SECRET_KEY=os.environ.get('SECRET_KEY', default='dev')
-    STORAGE_MODE = 'json'  # json ou sqlite
+    STORAGE_MODE = 'sqlite'  # json ou sqlite
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    SQLALCHEMY_DATABASE_URI = r'sqlite:///d:\python\examples\SampleFlask\app\db\clients.db'
+    #SQLALCHEMY_DATABASE_URI = r'sqlite:///d:\python\examples\SampleFlask\app\db\clients.db'
+    #Heroku
+    SQLALCHEMY_DATABASE_URI = r'sqlite:///app/db/clients.db'
