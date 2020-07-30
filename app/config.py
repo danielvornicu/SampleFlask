92c66e20db3374578d2825da1fa7a3afd2f3348b
danielvornicu <vdany2003@yahoo.com>
2020-07-30 18:00:44 +0200

    update

--- app/config.py
+++ app/config.py
@@ -2,6 +2,6 @@ import os
 
 class Config(object):
     SECRET_KEY=os.environ.get('SECRET_KEY', default='dev')
-    STORAGE_MODE = 'sqlite'  # json ou sqlite
+    STORAGE_MODE = 'json'  # json ou sqlite
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SQLALCHEMY_DATABASE_URI = r'sqlite:///d:\python\examples\SampleFlask\app\db\clients.db'
